01789e64034f71bf98878e3d97e67f591f508082
DevinduDh <3ddevindu@gmail.com>
2021-09-11 17:30:57 +0530

    Updated

--- periodic_signals.m
+++ periodic_signals.m
@@ -34,9 +34,33 @@ tiv=1/fs; %time interval between samples;
 t=0:tiv:(duy-tiv); %time intervals set
 y=square(wy*t); %signal data set
 plot(t,y,'k'); %plots figure
-axis([0 0.5 -1.5 1.5]);
+axis([0 duy -1.5 1.5]);
 %axis([0 0.03 -1.5 1.5]);
 xlabel('seconds'); title('square signal');
+
+%% Plotting a sawtooth wave
+
+fy = 100; %Signal frequency in Hz
+duy = 0.03; %signal duartion seconds
+fs = 20000; %sampling frequency in Hz
+t = 0:1/fs:0.05; %time interval set
+y = sawtooth(2*pi*fy*t); %signal data set
+plot(t,y);%plots figure
+axis([0 0.05 -1.5 1.5]);
+xlabel('t-->');
+title('sawtooth');
+
+%% triangular wave from sawtooth
+
+fy = 100; %signal frequency in Hz
+fs = 20000; %sample frequency
+t = 0:1/fs:0.05;%time interval set
+y = sawtooth(2*pi*fy*t,0.5); %signal data set
+plot(t,y); %plots figure
+axis([0 0.05 -1.5 1.5]);
+xlabel('t-->');
+title('triangular wave');
+
 
 
 
