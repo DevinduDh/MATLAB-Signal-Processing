3c1d9634aee4b17a8d71eac933fd94ae8b52516a
DevinduDh <3ddevindu@gmail.com>
2021-09-11 07:31:29 +0530

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MATLAB-Signal-Processing
+
+## Errors occured
+
+`Error using plot`
+`Vectors must be the same length.`
+
+Reason for this error is that number of elements in X axis is not equal to nmber of elements in Y axis or viseversa.
+
+Ex: Lets say you have 10 X values but have only 6 corresponding Y values.In this case you are unable to plot because you dont know some Y values for corresponding X value. The same thing goes with MATLAB. 
---
+++ sampling_squarewave.m
@@ -0,0 +1,23 @@
+%Sampling a Square signal
+% A=[1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,...
+% 1,1,1,1,1,0,0,0,0,0];
+% fs=10; %sampling frequency in Hz
+% tiv=1/fs; %time interval between samples;
+% t=0:tiv:(3-tiv); %time intervals set (30 values)
+% plot(t,A,'*'); %plots figure
+% axis([0 3 -0.5 1.5]);
+% xlabel('sec.'); title('square wave samples');
+
+% %% Sampling a triangular wave
+% A = [0,0.5,1,0.5,0,0.5,1,0.5,0];
+% %fs = 5;
+% tiv = 1;
+% t = 0:tiv:8;
+% plot(t,A);
+% axis([0 8 -1 1]); %axis defines the limits of two axis(xmin xmax ymin ymax)
+% xlabel('sec.');
+% title('triangular wave sample');
+
+
+
+
