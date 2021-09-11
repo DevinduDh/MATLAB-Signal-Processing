d5494a4a11dfd90437ca0125dc7910d335365694
DevinduDh <3ddevindu@gmail.com>
2021-09-11 10:59:20 +0530

    Updated

--- periodic_signals.m
+++ periodic_signals.m
@@ -26,9 +26,17 @@ axis([0 3 -1.5 1.5]);
 xlabel('seconds'); title('sine (solid) and cosine (dashed)');
 
 %% Plotting a square wave
-fy = 100; %Signal frequency
-t = 0:0.001:3; %time interval set
-y = square();
+fy=100; %signal frequency in Hz
+wy=2*pi*fy; %signal frequency in rad/s
+duy=0.02; %signal duration in seconds
+fs=20000; %sampling frequency in Hz
+tiv=1/fs; %time interval between samples;
+t=0:tiv:(duy-tiv); %time intervals set
+y=square(wy*t); %signal data set
+plot(t,y,'k'); %plots figure
+axis([0 0.5 -1.5 1.5]);
+%axis([0 0.03 -1.5 1.5]);
+xlabel('seconds'); title('square signal');
 
 
 
